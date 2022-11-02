1449db7c9e1679364b7d37d40cf84a500b9c7941
Aamir041 <aamirsaudagar34@gmail.com>
2022-11-02 05:46:23 +0530

    added Expense Filter

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,14 +1,31 @@
 import "./Expenses.css"
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter"
 import Card from "../UI/Card";
+import { useState } from "react";
 
 const Expense = (props) => {
+     const [filterYear, setFilterYear] = useState("2020")
+
+     const applyFilterHandler = (appliedFilterYear) => {
+          setFilterYear(appliedFilterYear);
+     }
+     console.log(filterYear);
+
      return(
           <Card className="expenses">
+               <ExpenseFilter 
+               selected = {filterYear}
+               applyFilterHandler={applyFilterHandler} 
+               />
                          {
                props.expenses.map(
                  (ele) => {
-                   return <ExpenseItem expense={ele} />
+                   return <ExpenseItem
+                        title = {ele.title}
+                        amount = {ele.amount}
+                        date = {ele.date}
+                        />
                    // here ele is individual element in array of object named expenses and that object is passes ad prop in <ExpenseIten />
                  }
                )
