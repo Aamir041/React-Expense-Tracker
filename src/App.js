d905f0a139b279f1aeed4f025b4329012b70cd7a
Aamir041 <aamirsaudagar34@gmail.com>
2022-11-03 18:22:51 +0530

    what did i add here ???

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import { v4} from "uuid"
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expense from "./components/Expenses/Expense";
 import { useState } from "react";
@@ -7,26 +7,26 @@ function App() {
   
   const [expenses,setExpenses] = useState([
     {
-      id: 'e1',
+      id: v4(),
       title: 'Note Book',
       amount: 4.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: v4(), title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: 'e3',
+      id: v4(),
       title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: 'e4',
+      id: v4(),
       title: 'New Desk (Wooden)',
       amount: 450,
       date: new Date(2021, 5, 12),
     },
     {
-      id: 'e5',
+      id: v4(),
       title: 'Vivo V5 (Plus)',
       amount: 300,
       date: new Date(2020, 6, 21),
